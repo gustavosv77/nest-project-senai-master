e9f6089bce91f19fb83d84f9575774861ee566d8
gustavosv77 <gustavosv.senai77@gmail.com>
2023-10-04 21:49:54 -0300

    feat: config validation auth create

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,6 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Matches, MinLength } from "class-validator";
+
 export class CreateAuthDto {
+    @IsString()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsString()
+    @MinLength(8, { message: 'Senha deve conter 8 digitos' })
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+      message: 'Senha muito fraca',
+    })
+    @ApiProperty({
+      example: '@Ab12345',
+      description:
+        'Senha deve conter 8 digitos, contendo: um simbolo, uma letra maiuscula e minuscula e um numero',
+    })
     password: string;
+    
+    @IsString()
     picture: string;
 }
