0588e41bea56b92cd1003f7e44673387bab60c4f
gustavosv77 <gustavosv.senai77@gmail.com>
2023-10-16 20:34:09 -0300

    refactor(auth): add error and add type in validateUser

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,12 +6,19 @@ import { UserService } from 'src/user/user.service';
 export class AuthService {
     constructor(private usersService: UserService) {}
 
-    async validateUser(email: string, password: string): Promise<any> {
+    async validateUser(email: string, password: string): Promise<
+    {
+        id: string;
+        email: string;
+        name: string;
+        picture: string;
+        admin?: boolean;
+    } | null> {
       const user: User = await this.usersService.findOne(email);
       if (user && user.password === password) {
         const { password, ...result } = user;
         return result;
       }
-      return null;
+      throw Error("Senha incorreta !");
     } 
 }
