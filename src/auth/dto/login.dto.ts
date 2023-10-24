8d92dc29d5bb062c6aaaaa563f62ab4c911d619c
gustavosv77 <gustavosv.senai77@gmail.com>
2023-10-24 19:05:05 -0300

    feat: add structure of the module and service telegram

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  @ApiProperty({ example: 'user32@emil.com' })
+  @ApiProperty({ example: 'user32@email.com' })
   email: string;
 
   @ApiProperty({ example: '@Ab12345' })
